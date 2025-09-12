06ebe10ed5d42f66ff615eaf1a2dfc2e53cb271f
marie hirte <mariehirte@Mac.fritz.box>
2025-09-12 16:58:29 +0200

    Add delete endpoint and error handling

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -53,5 +53,18 @@ def add_post():
     return jsonify(new_post), 201
 
 
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete(id):
+
+    post_to_delete = next((post for post in POSTS if post['id'] == id), None)
+
+    if post_to_delete is None:
+        return jsonify({"error": f"Post with id {id} not found"}), 404
+
+    POSTS.remove(post_to_delete)
+
+    return jsonify({"message": f"Post with id {id} has been deleted successfully."}), 200
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
